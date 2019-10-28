09d43aa1dcbc1cdd4c21276bfb24c302a7117536
Kilgore <conor6099@gmail.com>
2019-10-28 13:09:24 -0400

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Kilgore on 10/28/19.
+//  Copyright © 2019 Conor Kilgore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
